ff7b82a3cae51269a755c256893d9fd3dcde5ef2
Kristian Lange <k.lange@searchmetrics.com>
2017-06-13 17:00:32 +0200

    rm target

--- src/main/java/sample/jetty/web/SampleController.java
+++ src/main/java/sample/jetty/web/SampleController.java
@@ -16,6 +16,14 @@
 
 package sample.jetty.web;
 
+import org.asynchttpclient.AsyncHttpClient;
+import org.asynchttpclient.AsyncHttpClientConfig;
+import org.asynchttpclient.DefaultAsyncHttpClient;
+import org.asynchttpclient.DefaultAsyncHttpClientConfig;
+import org.asynchttpclient.ListenableFuture;
+import org.asynchttpclient.RequestBuilder;
+import org.asynchttpclient.Response;
+import org.asynchttpclient.proxy.ProxyServer;
 import org.springframework.beans.factory.annotation.Value;
 import sample.jetty.service.HelloWorldService;
 
@@ -24,16 +32,52 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.io.IOException;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+import static org.asynchttpclient.Dsl.asyncHttpClient;
+
 @Controller
 public class SampleController {
 
-	@Autowired
-	private HelloWorldService helloWorldService;
+    @Autowired
+    private HelloWorldService helloWorldService;
 
-	@GetMapping("/")
-	@ResponseBody
-	public String helloWorld() {
-		return this.helloWorldService.getHelloMessage();
-	}
+    @GetMapping("/")
+    @ResponseBody
+    public String helloWorld() throws IOException, ExecutionException, InterruptedException {
+        /*try(AsyncHttpClient asyncHttpClient = asyncHttpClient()) {
+            asyncHttpClient
+					.prepareGet("http://www.example.com/")
+					.execute()
+					.toCompletableFuture()
+					.thenApply(Response::getResponseBody)
+					.thenAccept(System.out::println)
+					.join();
+		}*/
+
+        AsyncHttpClientConfig cf = new DefaultAsyncHttpClientConfig.Builder()
+                .setUseOpenSsl(true).build();
+
+        AsyncHttpClient asyncHttpClient = new DefaultAsyncHttpClient(cf);
+
+        RequestBuilder requestBuilder =
+                new RequestBuilder("GET")
+                        .setUrl("http://www.euregex.eu/euregex-registers/euregex-b2c-adressermittlung/5/")
+                        .addHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8")
+                        .addHeader("Accept-Charset","utf-8")
+                        .addHeader("Accept-Encoding","gzip,deflate")
+                        .addHeader("Host", "www.euregex.eu")
+                        .addHeader("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.86 Safari/537.36")
+                        .setProxyServer(new ProxyServer.Builder("112.214.73.253", 80));
+        CompletableFuture<Response> responseFuture = asyncHttpClient.executeRequest(requestBuilder).toCompletableFuture();
+
+//        Future<Response> f =
+//                asyncHttpClient.prepareGet("https://www.euregex.eu/euregex-registers/euregex-b2c-adressermittlung/5/").execute();
+        Response r = responseFuture.get();
+        return r.getResponseBody();
+    }
 
 }
